314521486e95ae97cf5b4f2cf680943a8d8fe833
Alice <lehoangchauanh@gmail.com>
2024-08-07 15:42:53 +0700

    Add hyperlinks

--- src/sendgrid_mail.py
+++ src/sendgrid_mail.py
@@ -64,26 +64,23 @@ with open(csv_file_path, newline='') as csvfile:
         
         # Create the email content with the embedded image
         email_html_content = f"""
-        <html>
-            <body>
-                <div style="position: relative; display: inline-block;">
-
-                <table width="100%" height="100%" cellspacing="0" cellpadding="0">
-                    <tr>
-                        <td align="center" valign="middle">
-                        <img src="data:image/png;base64,{image_png}" usemap="#image-map" alt="Email Image" />
-                                                    <map name="image-map">
-<map name="image-map">
-    <area target="" alt="Link Sales 360" title="Link Sales 360" href="https://www.cs.cmu.edu/~akohlbre/" coords="813,93,701,17" shape="rect">
-    <area target="" alt="Link Central Point" title="Link Central Point" href="https://boldaugust.com/blog/how-to-make-a-logo-white-canva" coords="953,92,817,22" shape="rect">
-</map>                        </td>
-                    </tr>
-
-                </table>
+            <html>
+                <body>
+                    <div style="position: relative; display: inline-block;">
+                        <a href="https://www.w3schools.com">Visit Central Point</a>
+                        <br>
+                        <a href="https://www.w3schools.com">Visit 360</a>
+                        <table width="100%" height="100%" cellspacing="0" cellpadding="0">
+                            <tr>
+                                <td align="center" valign="middle">
+                                    <img src="data:image/png;base64,{image_png}" usemap="#image-map" alt="Email Image" />
+                                    
+                                </td>
+                            </tr>
+                        </table>
                     </div>
-
-            </body>
-        </html>
+                </body>
+            </html>
         """
         
         now = datetime.now()
@@ -91,6 +88,6 @@ with open(csv_file_path, newline='') as csvfile:
         subject = f"Sales dashboard - {row['Name']} - {now.strftime('%Y/%m/%d')}"
         print("Email: " + subject)
         # Send the email
-        # send_email(row['Email'], subject, email_html_content)
+        send_email(row['Email'], subject, email_html_content)
 
 
